4a4b3826225b7229ff0f58550696027c1f4a7af0
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2021-06-14 10:08:30 -0300

    queries

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,29 +1,50 @@
 ﻿using Microsoft.AspNet.OData;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using OData.Model;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace OData.Controllers
 {
     public class ProductsController : ODataController
     {
-        private List<Product> products = new List<Product>()
-        {
-            new Product()
-            {
-                ID = 1,
-                Name = "Bread",
-            }
-        };
-
         [EnableQuery]
         public List<Product> Get()
         {
             return products;
         }
+
+        private List<Product> products = new List<Product>()
+        {
+            new Product()
+            {
+                ID = 1,
+                Name = "Car",
+                Price = 25000
+            },
+            new Product()
+            {
+                ID = 2,
+                Name = "PlayStation",
+                Price = 2500
+            },
+            new Product()
+            {
+                ID = 3,
+                Name = "Bread",
+                Price = 1.5
+            },
+            new Product()
+            {
+                ID = 4,
+                Name = "Notebook",
+                Price = 5000
+            },
+            new Product()
+            {
+                ID = 5,
+                Name = "Shoes",
+                Price = 150
+            },
+        };
+
     }
 }
--- Model/Product.cs
+++ Model/Product.cs
@@ -9,5 +9,6 @@ namespace OData.Model
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public double Price { get; set; }
     }
 }
